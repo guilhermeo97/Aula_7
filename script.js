5c029095d3bc3453643ccf8d3f2c31cc7afc87dc
guilhermeo97 <oliveiraguilherme591@gmail.com>
2023-02-11 13:40:27 -0300

    init
    aula 7

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function potencia(numero, expoente) {
-    if(expoente === 0) {
+function potencia(numero, expoente){
+    if(expoente === 0){
         return 1;
     }
+
     let resultado = numero;
-    for(let i = 0; i < expoente; i++) {
+
+    if(expoente < 0){
+        for(let i = 1; i < (-expoente); i++){
+            resultado = resultado * numero;
+        }
+        return "1/" + resultado;
+    }
+
+    for(let i = 1; i < expoente; i++){
         resultado *= numero;
     }
+
     return resultado;
 }
 
-alert(potencia(3,2));
-
-
-
-
-
-
-
-
-
-
+alert(potencia(5, -3));
 
 
 
